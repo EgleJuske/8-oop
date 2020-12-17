99a1923fd45e1e8fe8c1381d69548a466ce50149
Eglė Juškevičienė <88egle@gmail.com>
2020-12-17 12:24:16 +0200

    oop idea

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// OBJEKTAS
+// - parametru rinkinys
+// - funkcionalumo rinkinys
+
+/*
+
+Animal.js (params: name, color, sound; methods: voice();)
+    Pet.js (paveldi is Animal.js)
+        Dog.js (paveldi is Pet.js)
+        Cat.js (paveldi is Pet.js)
+        Hamster.js (paveldi is Pet.js; params: canEatTillDeath)
+
+    Bird.js (paveldi is Animal.js; params: wingsCount, canFly; methods: flyAway();)
+        Chicken.js (viska paveldi is Bird.js)
+        Parrot.js (viska paveldi is Bird.js)
+        Eagle.js (viska paveldi is Bird.js)
+        Dragonfly.js (viska paveldi is Bird.js; params: imposter=true;)
+
+
+    Fish.js (paveldi is Animal.js; params: canSwim; methods: swimAway();)
+        Goldfish.js (viska paveldi is Fish.js; params: grantsWishes; methods: makeWish();)
+        Nemo.js (viska paveldi is Fish.js)
+
+*/
